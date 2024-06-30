fb566c47b4fdf67153294051a04b3e003356625d
里見 灯花 <wjqserverstudio@gmail.com>
2024-06-30 13:56:08 +0800

    Update and rename tools-cn.sh to tools-stable-cn.sh

--- tools-stable-cn.sh
+++ tools-stable-cn.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 # By WJQSERVER-STUDIO_WJQSERVER
-#https://github.com/WJQSERVER/tools-dev
+#https://github.com/WJQSERVER-STUDIO/tools-stable
 
 # 显示免责声明
 echo "免责声明：请阅读并同意以下条款才能继续使用本程序。"
 echo "本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
 echo "使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
-echo "当前版本为V.1.0"
+echo "当前版本为V.0.9"
 
 sleep 1
 
 #BETA版
-repo_url="https://gh.1888866.xyz/https://raw.githubusercontent.com/WJQSERVER/tools-dev/BETA/"
+repo_url="https://gh.1888866.xyz/https://raw.githubusercontent.com/WJQSERVER-STUDIO/tools-stable/main"
 echo "repo_url=$repo_url" > "repo_url.conf"
 
 # 导入配置文件

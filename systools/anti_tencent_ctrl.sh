3da0163010cfc00e106e3ddfd4c6147d504e5c67
里見 灯花 <172008506+satomitoka@users.noreply.github.com>
2024-07-01 15:47:35 +0800

    更新與改進
    添加詳細描述

--- systools/anti_tencent_ctrl.sh
+++ systools/anti_tencent_ctrl.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 #删除腾讯云控,保护隐私安全
 
+echo -e "[${yellow}RUN${white}] $mikublue 開始刪除騰訊雲控" $whit
+
+sleep 1
+
+echo -e "${green}>${white} $mikublue 停止進程" $white
 systemctl stop tat_agent
 systemctl disable tat_agent
 
+echo -e "${green}>${white} $mikublue 運行卸載程序" $white
 /usr/local/qcloud/stargate/admin/uninstall.sh
 /usr/local/qcloud/YunJing/uninst.sh
 /usr/local/qcloud/monitor/barad/admin/uninstall.sh
 
+echo -e "${green}>${white} $mikublue 刪除殘留" $white
 rm -f /etc/systemd/system/tat_agent.service
 rm -rf /usr/local/qcloud
 rm -rf /usr/local/sa
@@ -22,3 +29,5 @@ do
     kill -9 $A
   done
 done
+
+echo -e "[${green}OK${white}] $mikublue 騰訊雲控卸載完成" $white

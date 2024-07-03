ae458f12accd2e9f1a2ca1ef434e0aea08755d5d
WJQSERVER <wjqserver@outlook.com>
2024-07-04 02:17:24 +0800

    fix-test(BETA)

--- template/mainmenu.sh
+++ template/mainmenu.sh
@@ -97,25 +97,25 @@ function start_menu(){
     read -p "请输入数字:" menuNumberInput
     case "$menuNumberInput" in
         1 )
-           #
+           1
 	    ;;
         2 )
-	       #
+	       2
         ;;
 	    3 )
-           #
+           3
 	    ;;
         4 )
-	       #
+	       4
         ;;
 	    5 )
-           #
+           5
 	    ;;
         6 )
-	       #
+	       6
         ;;
 	    7 )
-           #
+           7
 	    ;;   
 
         0 )

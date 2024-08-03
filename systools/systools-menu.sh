b4a4c5c7ed2dd9a72c02537180bec8d30410d818
WJQSERVER <wjqserver@outlook.com>
2024-08-04 07:14:26 +0800

    0.9

--- systools/systools-menu.sh
+++ systools/systools-menu.sh
@@ -2,6 +2,21 @@
 # By WJQSERVER-STUDIO_WJQSERVER
 #https://github.com/WJQSERVER/tools-stable
 
+clear
+
+# 显示免责声明
+echo -e "${red}免责声明：${mikublue}请阅读并同意以下条款才能继续使用本脚本。"
+echo -e "${yellow}===================================================================="
+echo -e "${mikublue}本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
+echo -e "${mikublue}使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
+echo -e "${mikublue}不提供/保证任何功能的可用性，安全性，有效性，合法性"
+echo -e "${mikublue}当前版本为${white}  [${yellow} V.0.9 ${white}]  ${white}"
+echo -e "${yellow}===================================================================="
+
+# 导入配置文件
+source "repo_url.conf"
+sleep 1
+
 mikublue="\033[38;2;57;197;187m"
 yellow='\033[33m'
 white='\033[0m'
@@ -10,20 +25,13 @@ blue='\033[0;34m'
 red='\033[31m'
 gray='\e[37m'
 
-clear
-
-# 显示免责声明
-echo -e "${red}免责声明：请阅读并同意以下条款才能继续使用本脚本。"
-echo -e "${yellow}本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
-echo -e "${yellow}使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
-echo -e "${yellow}不提供/保证任何功能的可用性，安全性，有效性，合法性${white}"
-
-sleep 1
-
-# 导入配置文件
-source "repo_url.conf"
-
 #彩色
+mikublue(){
+    echo -e "\033[38;2;57;197;187m\033[01m$1\033[0m"
+}
+white(){
+    echo -e "\033[0m\033[01m$1\033[0m"
+}
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
@@ -35,6 +43,12 @@ yellow(){
 }
 blue(){
     echo -e "\033[34m\033[01m$1\033[0m"
+}
+gray(){
+    echo -e "\e[37m\033[01m$1\033[0m"
+}
+option(){
+    echo -e "\033[32m\033[01m ${1}. \033[38;2;57;197;187m${2}\033[0m"
 }
 
 #修改主机名
@@ -107,29 +121,28 @@ function back(){
 #主菜单
 function start_menu(){
     clear
-    yellow " WJQserver Studio的快捷工具箱 BETA版 "
-    green " WJQserver Studio tools BETA" 
-    yellow " FROM: https://github.com/WJQSERVER/tools-dev "
-    green " USE:  wget -O tools.sh ${repo_url}tools.sh && chmod +x tools.sh && clear && ./tools.sh "
-    yellow " =================================================="
-    green " 1. 修改主机名"
-    green " 2. 修改时区"
-    green " 3. 修改密码"
-    green " 4. 修改SSH配置"
-    green " 5. 用户管理"
-    yellow " =================================================="
-    green " 6. 修改网络配置&BBR"
-    green " 7. 修改日志配置"
-    green " 8. 防火墙配置"
-    yellow " =================================================="
-    green " 9. SWAP配置"
-    yellow " =================================================="
-    green " 10.解除腾讯云控"
-    yellow " =================================================="
-    green " 90.一键DD系统"
-    green " 99.重启"
-    yellow " =================================================="
-    green " 0. 返回主脚本"
+    red " WJQserver Studio Linux工具箱"
+    yellow " FROM: https://github.com/WJQSERVER-STUDIO/tools-stable "
+    green " =================================================="
+    option 1 "修改主机名"
+    option 2 "修改时区"
+    option 3 "修改密码"
+    option 4 "修改SSH配置"
+    option 5 "用户管理"
+    green " =================================================="
+    option 6 "修改网络配置&BBR"
+    option 7 "修改日志配置"
+    option 8 "防火墙配置"
+    green " =================================================="
+    option 9 "SWAP配置"
+    green " =================================================="
+    option 10 "解除腾讯云控"
+    green " =================================================="
+    option 90 "一键DD系统"
+    option 99 "重启"
+    green " =================================================="
+    option 0 "返回主脚本"
+
     echo
     read -p "请输入数字:" menuNumberInput
     case "$menuNumberInput" in

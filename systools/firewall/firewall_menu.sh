9ab3a20fc954f9c895b262555810111ce8d0f0bf
WJQSERVER <wjqserver@outlook.com>
2024-08-04 09:43:10 +0800

    0.9

--- systools/firewall/firewall_menu.sh
+++ systools/firewall/firewall_menu.sh
@@ -1,19 +1,25 @@
 #! /bin/bash
 # By WJQSERVER-STUDIO_WJQSERVER
-#https://github.com/WJQSERVER/tools-dev
-
-clear
-
-# 显示免责声明
-echo "免责声明：请阅读并同意以下条款才能继续使用本脚本。"
-echo "本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
-echo "使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
+#https://github.com/WJQSERVER/tools-stable
 
 # 导入配置文件
 source "repo_url.conf"
-sleep 1
+
+mikublue="\033[38;2;57;197;187m"
+yellow='\033[33m'
+white='\033[0m'
+green='\033[0;32m'
+blue='\033[0;34m'
+red='\033[31m'
+gray='\e[37m'
 
 #彩色
+mikublue(){
+    echo -e "\033[38;2;57;197;187m\033[01m$1\033[0m"
+}
+white(){
+    echo -e "\033[0m\033[01m$1\033[0m"
+}
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
@@ -26,6 +32,24 @@ yellow(){
 blue(){
     echo -e "\033[34m\033[01m$1\033[0m"
 }
+gray(){
+    echo -e "\e[37m\033[01m$1\033[0m"
+}
+option(){
+    echo -e "\033[32m\033[01m ${1}. \033[38;2;57;197;187m${2}\033[0m"
+}
+
+clear
+
+# 显示免责声明
+echo -e "${red}免责声明：${mikublue}请阅读并同意以下条款才能继续使用本脚本。"
+echo -e "${yellow}===================================================================="
+echo -e "${mikublue}本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
+echo -e "${mikublue}使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
+echo -e "${mikublue}不提供/保证任何功能的可用性，安全性，有效性，合法性"
+echo -e "${mikublue}当前版本为${white}  [${yellow} V.0.9 ${white}]  ${white}"
+echo -e "${yellow}===================================================================="
+sleep 1
 
 #UFW
 function ufw(){
--- systools/network/network_menu.sh
+++ systools/network/network_menu.sh
@@ -1,19 +1,25 @@
 #! /bin/bash
 # By WJQSERVER-STUDIO_WJQSERVER
-#https://github.com/WJQSERVER/tools-dev
-
-clear
-
-# 显示免责声明
-echo "免责声明：请阅读并同意以下条款才能继续使用本脚本。"
-echo "本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
-echo "使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
+#https://github.com/WJQSERVER/tools-stable
 
 # 导入配置文件
 source "repo_url.conf"
-sleep 1
+
+mikublue="\033[38;2;57;197;187m"
+yellow='\033[33m'
+white='\033[0m'
+green='\033[0;32m'
+blue='\033[0;34m'
+red='\033[31m'
+gray='\e[37m'
 
 #彩色
+mikublue(){
+    echo -e "\033[38;2;57;197;187m\033[01m$1\033[0m"
+}
+white(){
+    echo -e "\033[0m\033[01m$1\033[0m"
+}
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
@@ -26,6 +32,24 @@ yellow(){
 blue(){
     echo -e "\033[34m\033[01m$1\033[0m"
 }
+gray(){
+    echo -e "\e[37m\033[01m$1\033[0m"
+}
+option(){
+    echo -e "\033[32m\033[01m ${1}. \033[38;2;57;197;187m${2}\033[0m"
+}
+
+clear
+
+# 显示免责声明
+echo -e "${red}免责声明：${mikublue}请阅读并同意以下条款才能继续使用本脚本。"
+echo -e "${yellow}===================================================================="
+echo -e "${mikublue}本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
+echo -e "${mikublue}使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
+echo -e "${mikublue}不提供/保证任何功能的可用性，安全性，有效性，合法性"
+echo -e "${mikublue}当前版本为${white}  [${yellow} V.0.9 ${white}]  ${white}"
+echo -e "${yellow}===================================================================="
+sleep 1
 
 #
 function networkinfo(){
@@ -60,18 +84,16 @@ function back(){
 #主菜单
 function start_menu(){
     clear
-    yellow " WJQserver Studio 工具箱 Stable"
-    green " WJQserver Studio tools-stable" 
-    yellow " FROM: https://github.com/WJQSERVER/tools-stable "
-    green " USE:  wget -O tools.sh ${repo_url}tools.sh && chmod +x tools.sh && clear && ./tools.sh "
-    yellow " =================================================="
-    green " 1. 網路側信息查看" 
-    green " 2. IPv4/IPv6優先級切換"
-    green " 3. BBR管理面板"
-    green " 4. 更換DNS" 
-    green " 5. 檢測端口占用"
-    yellow " =================================================="
-    green " 0. 返回主脚本"
+    red " WJQserver Studio Linux工具箱"
+    yellow " FROM: https://github.com/WJQSERVER-STUDIO/tools-stable "
+    green " =================================================="
+    option 1 "網路側信息查看" 
+    option 2 "IPv4/IPv6優先級切換"
+    option 3 "BBR管理面板"
+    option 4 "更換DNS"
+    option 5 "檢測端口占用"
+    green " =================================================="
+    option 0 "返回主脚本"
     echo
     read -p "请输入数字:" menuNumberInput
     case "$menuNumberInput" in

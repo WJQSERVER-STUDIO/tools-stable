7ad22ef9923d8e0162ab91d054c03b2e7599e3c3
WJQSERVER <wjqserver@outlook.com>
2024-08-29 09:10:23 +0800

    0.9.3

--- tools-stable-ghproxy.sh
+++ tools-stable-ghproxy.sh
@@ -2,6 +2,16 @@
 # By WJQSERVER-STUDIO_WJQSERVER
 #https://github.com/WJQSERVER/tools-stable
 
+# 获取当前版本
+version=$(curl -s https://raw.githubusercontent.com/WJQSERVER-STUDIO/tools-stable/main/Version)
+
+# 获取统计信息
+total=$(curl -s https://count.1888866.xyz/api/counter/total)
+today=$(curl -s https://count.1888866.xyz/api/counter/daily)
+
+# 统计次数
+curl -s https://count.1888866.xyz/add
+
 mikublue="\033[38;2;57;197;187m"
 yellow='\033[33m'
 white='\033[0m'
@@ -18,7 +28,8 @@ echo -e "${yellow}==============================================================
 echo -e "${mikublue}本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
 echo -e "${mikublue}使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
 echo -e "${mikublue}不提供/保证任何功能的可用性，安全性，有效性，合法性"
-echo -e "${mikublue}当前版本为${white}  [${yellow} V.0.9 ${white}]  ${white}"
+echo -e "${mikublue}当前版本为${white}  [${yellow} V.${version} ${white}]  ${white}"
+echo -e "${mikublue}本脚本已运行${yellow}${total}${mikublue}次 今日运行${yellow}${today}${mikublue}次"
 echo -e "${yellow}===================================================================="
 
 sleep 1

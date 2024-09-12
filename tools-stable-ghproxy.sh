8760cf120012f8477a8dbfd207adc0fdf5653bf8
里見 灯花 <172008506+satomitoka@users.noreply.github.com>
2024-09-12 16:56:56 +0800

    fix

--- tools-stable-ghproxy.sh
+++ tools-stable-ghproxy.sh
@@ -2,15 +2,33 @@
 # By WJQSERVER-STUDIO_WJQSERVER
 #https://github.com/WJQSERVER/tools-stable
 
-# 获取当前版本
-version=$(curl -s https://raw.githubusercontent.com/WJQSERVER-STUDIO/tools-stable/main/Version)
+#Stable版 ghproxy
+repo_url="https://ghproxy0rtt.1888866.xyz/raw.githubusercontent.com/WJQSERVER-STUDIO/tools-stable/main/"
 
-# 获取统计信息
-total=$(curl -s https://count.1888866.xyz/api/counter/total)
-today=$(curl -s https://count.1888866.xyz/api/counter/daily)
+# 获取当前版本并设置3秒超时
+version=$(curl -s --max-time 3 ${repo_url}Version)
+if [ $? -ne 0 ]; then
+    version="unknown"  # 设置默认值或进行其他错误处理
+fi
+
+# 获取统计信息并设置3秒超时
+total=$(curl -s --max-time 3 https://count.1888866.xyz/api/counter/total)
+if [ $? -ne 0 ]; then
+    total="unknown"  # 设置默认值或进行其他错误处理
+fi
+
+today=$(curl -s --max-time 3 https://count.1888866.xyz/api/counter/daily)
+if [ $? -ne 0 ]; then
+    today="unknown"  
+fi
 
 # 统计次数
-curl -s https://count.1888866.xyz/add
+response=$(curl -s --max-time 3 https://count.1888866.xyz/add)
+if [ $? -ne 0 ]; then
+    echo 
+else
+    echo 
+fi
 
 mikublue="\033[38;2;57;197;187m"
 yellow='\033[33m'
@@ -35,9 +53,6 @@ echo -e "${yellow}==============================================================
 sleep 1
 
 conf_file="repo_url.conf"
-
-#Stable版
-repo_url="https://ghproxy-go.1888866.xyz/raw.githubusercontent.com/WJQSERVER-STUDIO/tools-stable/main/"
 
 echo "repo_url=$repo_url" > "$conf_file"
 

748b7b64388fef2df3e599899b0bd8d8d446012e
里見 灯花 <172008506+satomitoka@users.noreply.github.com>
2024-09-17 10:47:57 +0800

    Update docker-manager-menu.sh

--- docker-manager/docker-manager-menu.sh
+++ docker-manager/docker-manager-menu.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 # By WJQSERVER-STUDIO_WJQSERVER
-#https://github.com/WJQSERVER/tools-dev
-
-clear
+#https://github.com/WJQSERVER-STUDIO/tools-stable
 
 # 导入配置文件
 source "repo_url.conf"
@@ -40,6 +38,23 @@ gray(){
 option(){
     echo -e "\033[32m\033[01m ${1}. \033[38;2;57;197;187m${2}\033[0m"
 }
+
+version=$(curl -s --max-time 3 ${repo_url}Version)
+if [ $? -ne 0 ]; then
+    version="unknown"  
+fi
+
+clear
+
+# 显示免责声明
+echo -e "${red}免责声明：${mikublue}请阅读并同意以下条款才能继续使用本脚本。"
+echo -e "${yellow}===================================================================="
+echo -e "${mikublue}本脚本仅供学习和参考使用，作者不对其完整性、准确性或实用性做出任何保证。"
+echo -e "${mikublue}使用本脚本所造成的任何损失或损害，作者不承担任何责任。"
+echo -e "${mikublue}不提供/保证任何功能的可用性，安全性，有效性，合法性"
+echo -e "${mikublue}当前版本为${white}  [${yellow} V${version} ${white}]  ${white}"
+echo -e "${yellow}===================================================================="
+sleep 1
 
 #Docker一键安装&更新
 function docker-install(){
